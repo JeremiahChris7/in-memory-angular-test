fa8d6b62ca13281e4b95a94b0afad6b8f339aab9
Jeremiah <jeremiahchris7@gmail.com>
2019-08-07 21:02:28 -0600

    commit finished angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedLink: string;
+  title = 'Tour of Heroes';
+
+  constructor() {
+    this.selectedLink = '/dashboard';
+  }
+
+  onSelect(link: string): void {
+    this.selectedLink = link;
+  }
 }
